a8f4405b02e80c276eacdb1217664fcef76e474d
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-14 13:35:23 +0600

    user data data insert

--- QUERY.sql
+++ QUERY.sql
@@ -1,6 +1,6 @@
---  database name
+--  database create
 create database bookingsystem
---  user table
+--  user table create
 create table users(
   users_id serial primary key,
   full_name  varchar(50) not null,
@@ -9,3 +9,11 @@ create table users(
   role varchar(20) check(role in ('Ticket Manager ','Football Fan')) not null
    
 )
+
+  -- user data insert 
+insert into Users(user_id,full_name,email,role,phone_number)
+  values
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
