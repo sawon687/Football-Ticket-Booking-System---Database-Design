96ff976a058ec585b2755b058a9394dbad4a7f97
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-06-15 10:37:26 +0600

    added 4  qustion ansaers

--- QUERY.sql
+++ QUERY.sql
@@ -77,3 +77,8 @@ where full_name ilike 'Tanvir%' or full_name ilike '%Haque%'
 -- qustion 3 ans
 select booking_id,user_id,match_id, coalesce(payment_status,'Action Required') as systematic_status from bookings
 where payment_status is null 
+
+--- qustion 4 ans
+select booking_id ,full_name ,fixture,total_cost from bookings
+inner join users on bookings.user_id = users.user_id
+inner join matches on matches.match_id=bookings.match_id
